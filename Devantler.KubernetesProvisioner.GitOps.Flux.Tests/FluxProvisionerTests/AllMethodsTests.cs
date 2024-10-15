bc91c61e14e76a35df0720540081e6ef8665da0d
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-15 19:19:03 +0200

    Added FluxProvisioner

---
+++ Devantler.KubernetesProvisioner.GitOps.Flux.Tests/FluxProvisionerTests/AllMethodsTests.cs
@@ -0,0 +1,34 @@
+using Devantler.KindCLI;
+
+namespace Devantler.KubernetesProvisioner.GitOps.Flux.Tests.FluxProvisionerTests;
+
+/// <summary>
+/// Tests for all methods in the <see cref="FluxProvisioner"/> class.
+/// </summary>
+[Collection("Flux")]
+public class AllMethodsTests
+{
+  /// <summary>
+  /// Test to verify that flux installs and reconciles kustomizations.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task Flux_InstallsAndReconciles_KustomizationsAsync()
+  {
+    // Arrange
+    string clusterName = "test-flux-cluster";
+    string configPath = Path.Combine(AppContext.BaseDirectory, "assets/kind-config.yaml");
+    var fluxProvisioner = new FluxProvisioner("kind-" + clusterName);
+    var cancellationToken = new CancellationToken();
+
+    // Act
+    await Kind.DeleteClusterAsync(clusterName, cancellationToken);
+    await Kind.CreateClusterAsync(clusterName, configPath, cancellationToken);
+    await fluxProvisioner.InstallAsync(cancellationToken);
+    await fluxProvisioner.ReconcileAsync(cancellationToken);
+    await fluxProvisioner.UninstallAsync(cancellationToken);
+
+    // Cleanup
+    await Kind.DeleteClusterAsync(clusterName, cancellationToken);
+  }
+}
---
+++ Devantler.KubernetesProvisioner.GitOps.Flux/FluxProvisioner.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Json;
+using Devantler.KubernetesProvisioner.GitOps.Core;
+using Devantler.KubernetesProvisioner.Resources.Native;
+using k8s;
+using k8s.Models;
+
+namespace Devantler.KubernetesProvisioner.GitOps.Flux;
+
+/// <summary>
+/// A Kubernetes GitOps provisioner using Flux.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="FluxProvisioner"/> class.
+/// </remarks>
+/// <param name="context"></param>
+public class FluxProvisioner(string? context = default) : IKubernetesGitOpsProvisioner
+{
+
+  /// <inheritdoc/>
+  public string? Context { get; set; } = context;
+
+  /// <summary>
+  /// Install Flux on the Kubernetes cluster.
+  /// </summary>
+  /// <param name="cancellationToken"></param>
+  /// <returns></returns>
+  public async Task InstallAsync(CancellationToken cancellationToken = default) =>
+    await FluxCLI.Flux.InstallAsync(Context, cancellationToken).ConfigureAwait(false);
+
+  /// <summary>
+  /// Reconcile resources on the Kubernetes cluster.
+  /// </summary>
+  /// <param name="cancellationToken"></param>
+  /// <returns></returns>
+  public async Task ReconcileAsync(CancellationToken cancellationToken = default)
+  {
+    using var kubernetesResourceProvisioner = new KubernetesResourceProvisioner(Context);
+    var kustomizations = await kubernetesResourceProvisioner.CustomObjects.ListNamespacedCustomObjectAsync<IEnumerable<V1CustomResourceDefinition>>("kustomize.toolkit.fluxcd.io", "v1", "flux-system", "kustomizations", cancellationToken: cancellationToken).ConfigureAwait(false);
+    foreach (var kustomization in kustomizations)
+    {
+      await FluxCLI.Flux.ReconcileKustomizationAsync(kustomization.Metadata.Name, Context, withSource: true, cancellationToken: cancellationToken).ConfigureAwait(false);
+    }
+  }
+
+  /// <summary>
+  /// Uninstall Flux from the Kubernetes cluster.
+  /// </summary>
+  /// <param name="cancellationToken"></param>
+  /// <returns></returns>
+  public async Task UninstallAsync(CancellationToken cancellationToken = default) =>
+    await FluxCLI.Flux.UninstallAsync(Context, cancellationToken).ConfigureAwait(false);
+}
