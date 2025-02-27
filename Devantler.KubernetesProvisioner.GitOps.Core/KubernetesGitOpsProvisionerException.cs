42e8f37ef6e262f92b0745e6272ddccfec3d6e0c
Nikolai Emil Damm <neq@energinet.dk>
2025-02-27 09:02:15 +0100

    feat(tests): add unit tests for KubernetesClusterProvisionerException and KubernetesGitOpsProvisionerException
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

---
+++ Devantler.KubernetesProvisioner.GitOps.Core/KubernetesGitOpsProvisionerException.cs
@@ -0,0 +1,32 @@
+namespace Devantler.KubernetesProvisioner.GitOps.Core;
+
+/// <summary>
+/// Represents an exception that is thrown when an error occurs in a Kubernetes GitOps provisioner.
+/// </summary>
+[Serializable]
+public class KubernetesGitOpsProvisionerException : Exception
+{
+  /// <summary>
+  /// Initializes a new instance of the <see cref="KubernetesGitOpsProvisionerException"/> class.
+  /// </summary>
+  public KubernetesGitOpsProvisionerException()
+  {
+  }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="KubernetesGitOpsProvisionerException"/> class with a specified error message.
+  /// </summary>
+  /// <param name="message"></param>
+  public KubernetesGitOpsProvisionerException(string? message) : base(message)
+  {
+  }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="KubernetesGitOpsProvisionerException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+  /// </summary>
+  /// <param name="message"></param>
+  /// <param name="innerException"></param>
+  public KubernetesGitOpsProvisionerException(string? message, Exception? innerException) : base(message, innerException)
+  {
+  }
+}
