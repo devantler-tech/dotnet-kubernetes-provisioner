9b2e039b81f0acb3e73b53c86e12d472b2652ff2
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-03-18 16:06:24 +0100

    fix: correct context initialization in CiliumProvisionerTests
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- tests/Devantler.KubernetesProvisioner.CNI.Cilium.Tests/CiliumProvisionerTests/AllMethodsTests.cs
+++ tests/Devantler.KubernetesProvisioner.CNI.Cilium.Tests/CiliumProvisionerTests/AllMethodsTests.cs
@@ -24,7 +24,7 @@ public class AllMethodsTests
     // Arrange
     string clusterName = "test-cilium-cluster";
     string context = "kind-" + clusterName;
-    CiliumProvisioner ciliumProvisioner = new(context: "kind-" + clusterName);
+    CiliumProvisioner ciliumProvisioner = new(context: context);
 
     string configPath = Path.Combine(AppContext.BaseDirectory, "assets/kind.yaml");
     var cancellationToken = new CancellationToken();
