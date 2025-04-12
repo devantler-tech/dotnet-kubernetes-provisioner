cffeefc6cc7c250f2322ce4cd7ea8ea2d8ae2029
Nikolai Emil Damm <ned@devantler.com>
2025-04-12 16:09:23 +0200

    Update KubectlProvisioner.cs

--- src/Devantler.KubernetesProvisioner.Deployment.Kubectl/KubectlProvisioner.cs
+++ src/Devantler.KubernetesProvisioner.Deployment.Kubectl/KubectlProvisioner.cs
@@ -61,7 +61,7 @@ public class KubectlProvisioner(string? kubeconfig = default, string? context = 
     };
     args.AddIfNotNull("--kubeconfig={0}", Kubeconfig);
     args.AddIfNotNull("--context={0}", Context);
-    var (exitCode, result) = await KubectlCLI.Kubectl.RunAsync([.. args], validation: CliWrap.CommandResultValidation.None, cancellationToken: cancellationToken).ConfigureAwait(false);
+    (exitCode, result) = await KubectlCLI.Kubectl.RunAsync([.. args], validation: CliWrap.CommandResultValidation.None, cancellationToken: cancellationToken).ConfigureAwait(false);
     if (exitCode != 0 && !result.Contains("error: no objects passed to apply", StringComparison.Ordinal))
     {
       throw new KubernetesDeploymentToolProvisionerException(result);
