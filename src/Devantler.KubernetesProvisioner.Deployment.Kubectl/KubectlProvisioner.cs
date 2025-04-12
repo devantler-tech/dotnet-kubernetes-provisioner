6381129dde7ed19e50bdd5d4f2c8ac3049127318
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-04-12 10:25:26 +0200

    fix: remove unnecessary '--all' argument from kubectl apply command in PushAsync method
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/Devantler.KubernetesProvisioner.Deployment.Kubectl/KubectlProvisioner.cs
+++ src/Devantler.KubernetesProvisioner.Deployment.Kubectl/KubectlProvisioner.cs
@@ -34,7 +34,6 @@ public class KubectlProvisioner(string? kubeconfig = default, string? context = 
       "-l=provider=ksail"
       $"--timeout={timeout}",
       "--prune",
-      "--all",
       "--wait=true",
     };
     args.AddIfNotNull("--kubeconfig={0}", Kubeconfig);
